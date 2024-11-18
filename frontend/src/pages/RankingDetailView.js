bc7e5be717084941d4c0709e375eae6958ac8ffb
Tobias Unterhauser <tobias.unterhauser@tum.de>
2024-11-18 22:25:16 +0100

    refactored homepage and created first iteraiton of ranking table

---
+++ frontend/src/pages/RankingDetailView.js
@@ -0,0 +1,15 @@
+import {Box} from "@mui/joy";
+
+const RankingDetailView = () => {
+
+    //todo: fetch respective ranking from backend by id
+
+    return (
+        <Box>
+            <h1>Rankings Detail Page</h1>
+            <p>Show Details about privacy method here</p>
+        </Box>
+    );
+}
+
+export default RankingDetailView;
