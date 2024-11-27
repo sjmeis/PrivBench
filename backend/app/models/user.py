207c94e59de7b08445f19223b85a7e516e501a2e
Tobias Unterhauser <tobias.unterhauser@tum.de>
2024-11-27 14:33:52 +0100

    added result model to store privatization results, with respective connection to user model

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,11 +1,22 @@
-from .. import db
+from ..extensions import db
 from werkzeug.security import generate_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    #mailAddress = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+    badges = db.Column(db.PickleType, nullable=True)
+    #researchInstitute = db.Column(db.String(120), nullable=True)
+
+
+    # The relationship from User to Result
+    # One user can have many results
+    results = db.relationship('Result', backref='user', lazy=True)
 
     def __init__(self, username, password):
         self.username = username
+        #self.mailAddress = mailAddress
         self.password = generate_password_hash(password)
+        #self.badges = badges
+        #self.researchInstitute = researchInstitute
---
+++ backend/app/routes/ranking.py
@@ -0,0 +1,73 @@
+from flask import Blueprint, request, jsonify, make_response, current_app
+from ..models import Result, User
+from .. import db
+
+ranking_bp = Blueprint('ranking', __name__)
+
+@ranking_bp.route('/ranking', methods=['GET'])
+def ranking():
+    results = Result.query.order_by(Result.score.desc()).all()
+    results_list = [
+        {
+            "id": result.id,
+            "submission_date": result.submission_date.isoformat(),
+            "name": result.name,
+            "method": result.method,
+            "submitted_by": result.submitted_by,
+            "score": result.score
+        }
+        for result in results
+    ]
+    return jsonify(results_list)
+
+@ranking_bp.route('/ranking', methods=['POST'])
+def get_all_filtered():
+    try:
+        data = request.get_json()
+        search_term = data.get('searchTerm', '')
+        page = data.get('page', 1)
+        limit = data.get('limit', 8)
+
+        query = db.session.query(Result).join(User).order_by(Result.score.desc())
+
+        if search_term:
+            search_term = f"%{search_term}%"
+            query = query.filter(
+                or_(
+                    Result.name.ilike(search_term),
+                    Result.method.ilike(search_term),
+                    User.username.ilike(search_term)
+                )
+            )
+
+        offset = (page - 1) * limit
+        results = query.offset(offset).limit(limit).all()
+
+        total = query.count()
+
+        results_list = [
+            {
+                "id": result.id,
+                "submissionDate": result.submission_date.isoformat(),
+                "name": result.name,
+                "method": result.method,
+                "submittedBy": {
+                    "id": result.submitted_by,
+                    "username": result.user.username,
+                    "badges": result.user.badges
+                },
+                "score": result.score
+            }
+            for result in results
+        ]
+
+        response = {
+            "results": results_list,
+            "totalPages": (total + limit - 1) // limit,  # Calculate total pages
+            "currentPage": page
+        }
+        return jsonify(response), 200
+
+    except Exception as e:
+        return jsonify({"message": "Internal server error", "error": str(e)}), 500
+
