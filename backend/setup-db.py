bcbd15b86a8d37cf535c715dd0188ddb03be0c1d
Tobias Unterhauser <tobias.unterhauser@tum.de>
2025-01-29 23:44:16 +0100

    files file naming and setup script for 3 modules

--- backend/setup-db.py
+++ backend/setup-db.py
@@ -1,6 +1,6 @@
 from app import create_app, db
 from app.models import Dataset, BenchmarkModule
-from app.utils.module_manager import ModuleManager
+from app.tasks.add_module import install_and_load_module
 from datetime import datetime
 import os
 import logging
@@ -28,83 +28,80 @@ app = create_app()
 
 with app.app_context():
     # Drop all tables and recreate them
-    # db.drop_all()
-    # db.create_all()
+    db.drop_all()
+    db.create_all()
 
-    module_names = ['NERpriv']
-    module_file_names = ['ner_priv.py']
-    module_titles = ['NER Evaluation']
-    module_descriptions = [
-        "On the surface, text privatization should pay particular attention to named entities, or words or groups of words that point to some real-world object, person, organization, etc. Ensuring that such entities are not leaked into the privatized text, while also balancing the preservation of semantics, is the mark of an effective privatization method."
+    module_names = ['NERpriv', 'Coherence', 'NearestNeighbor']
+    module_file_names = ['NERpriv.py', 'Coherence.py',  'NearestNeighbor.py',]
+    module_requirement_file_names = [
+        'ner_requirements.txt', 'coh-reqs.txt',
+        'nearest-neighbor-reqs.txt'
     ]
 
-    manager = ModuleManager()
+    module_titles = [
+        'NER Evaluation', 'Text Coherence',
+        'Nearest Neighbor Search'
+    ]
+    module_descriptions = [
+        "On the surface, text privatization should pay particular attention to named entities, or words or groups of words that point to some real-world object, person, organization, etc. Ensuring that such entities are not leaked into the privatized text, while also balancing the preservation of semantics, is the mark of an effective privatization method.",
+        "This module evaluates the coherence of text, ensuring logical flow and semantic connectivity between sentences and paragraphs.",
+        "The nearest neighbor module enables efficient searching for the closest data points, useful in classification and recommendation systems."
+    ]
+    dataset_name = 'test_original.csv'
 
     # Iterate over all files in the dataset folder
-    if os.path.exists(DATASET_FOLDER):
-        for i, dataset_name in enumerate(os.listdir(DATASET_FOLDER)):
-            file_path = os.path.join(DATASET_FOLDER, dataset_name)
-            
-            # Check if the file exists and is a file (not a directory)
-            if os.path.isfile(file_path):
-                logger.info(f"Adding dataset: {dataset_name} at path: {file_path}")
-                
-                # Create a new Dataset entry
-                new_dataset = Dataset(
-                    name=dataset_name,
-                    file_path=file_path,
-                    created_at=datetime.utcnow(),
-                    is_active=True
-                )
+    if os.path.exists(DATASET_FOLDER) and os.path.exists(MODULE_FOLDER):
 
-                # Add and commit the new entry to the database
-                db.session.add(new_dataset)
-                db.session.flush()
+        file_path = os.path.join(DATASET_FOLDER, dataset_name)
+        if os.path.isfile(file_path):
+            logger.info(f"Adding dataset: {dataset_name} at path: {file_path}")
 
-                # Dynamically fetch the module name from the list
-                module_name = module_names[i % len(module_names)]
-                module_title = module_titles[i % len(module_titles)]
-                module_file_name = module_file_names[i % len(module_file_names)]
-                module_path = os.path.join(MODULE_FOLDER, module_file_name)
-                module_description = module_descriptions[i % len(module_descriptions)]
+            # Create a new Dataset entry
+            new_dataset = Dataset(
+                name=dataset_name,
+                file_path=file_path,
+                created_at=datetime.utcnow(),
+                is_active=True
+            )
 
-                logger.info(f"Adding module: {module_name} at path: {module_path}")
+            # Add and commit the new entry to the database
+            db.session.add(new_dataset)
+            db.session.flush()
 
-                # Determine the path to the requirements file
-                requirements_path = os.path.join(MODULE_FOLDER, 'module_requirements.txt')
+        for i, module_name in enumerate(module_names):
 
-                # Build and test the module container
-                try:
-                    image_tag = manager.build_module_container(
-                        module_id=new_dataset.id,
-                        module_path=module_path,
-                        module_name=module_name,
-                        requirements_path=requirements_path
-                    )
-                    test_result = manager.test_module(image_tag, module_name)
+            module_name = module_names[i % len(module_names)]
+            module_title = module_titles[i % len(module_titles)]
+            module_file_name = module_file_names[i % len(module_file_names)]
+            module_path = os.path.join(MODULE_FOLDER, module_file_name)
+            module_description = module_descriptions[i % len(module_descriptions)]
+            requirements_file_name = module_requirement_file_names[i % len(module_requirement_file_names)]
+            requirements_path = os.path.join(MODULE_FOLDER, requirements_file_name)
 
-                    if test_result["success"]:
-                        logger.info(f"Module {module_name} installed and tested successfully")
-                    else:
-                        logger.error(f"Module test failed: {test_result['error']}")
-                        continue  # Skip adding this module if test fails
+            logger.info(f"Adding module: {module_name} at path: {module_path}")
 
-                except Exception as e:
-                    logger.error(f"Failed to build or test module {module_name}: {str(e)}")
-                    continue
 
-                new_benchmark_module = BenchmarkModule(
-                    name=module_name,
-                    title=module_title,
-                    description=module_description,
-                    version="1.0.0",
-                    is_active=True,
-                    path=module_path,
-                    dataset_id=new_dataset.id
-                )
-                db.session.add(new_benchmark_module)
-        
-        # Commit all changes
+            new_benchmark_module = BenchmarkModule(
+                name=module_name,
+                title=module_title,
+                description=module_description,
+                version="1.0.0",
+                is_active=True,
+                path=module_path,
+                dataset_id=new_dataset.id
+            )
+
+            install_task = install_and_load_module.delay(
+                module_id=new_benchmark_module.id,
+                module_name=module_name,
+                module_path=module_path,
+                requirements_path=requirements_path
+            )
+
+            db.session.add(new_benchmark_module)
+
+            logger.info(f"Module {module_name} installed successfully")
+
         db.session.commit()
         logger.info("All datasets and modules have been added to the database.")
     else:
