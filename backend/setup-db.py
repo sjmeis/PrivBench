9545374fa8a46cdc747a840c438d8bb7b5ae66b4
Tobias Unterhauser <tobias.unterhauser@tum.de>
2025-01-30 00:02:58 +0100

    added another module

--- backend/setup-db.py
+++ backend/setup-db.py
@@ -31,21 +31,23 @@ with app.app_context():
     db.drop_all()
     db.create_all()
 
-    module_names = ['NERpriv', 'Coherence', 'NearestNeighbor']
-    module_file_names = ['NERpriv.py', 'Coherence.py',  'NearestNeighbor.py',]
+    module_names = ['NERpriv', 'Coherence', 'NearestNeighbor', 'Similarity']
+    module_file_names = ['NERpriv.py', 'Coherence.py',  'NearestNeighbor.py', 'Similarity.py']
     module_requirement_file_names = [
         'ner_requirements.txt', 'coh-reqs.txt',
-        'nearest-neighbor-reqs.txt'
+        'nearest-neighbor-reqs.txt', 'similarity-reqs.txt'
     ]
 
     module_titles = [
         'NER Evaluation', 'Text Coherence',
-        'Nearest Neighbor Search'
+        'Nearest Neighbor Search', 'Text Similarity'
     ]
     module_descriptions = [
         "On the surface, text privatization should pay particular attention to named entities, or words or groups of words that point to some real-world object, person, organization, etc. Ensuring that such entities are not leaked into the privatized text, while also balancing the preservation of semantics, is the mark of an effective privatization method.",
         "This module evaluates the coherence of text, ensuring logical flow and semantic connectivity between sentences and paragraphs.",
-        "The nearest neighbor module enables efficient searching for the closest data points, useful in classification and recommendation systems."
+        "The nearest neighbor module enables efficient searching for the closest data points, useful in "
+        "classification and recommendation systems.",
+        "Text similarity measures the likeness between two pieces of text, commonly used in search engines, clustering, and recommendation tasks."
     ]
     dataset_name = 'test_original.csv'
 
