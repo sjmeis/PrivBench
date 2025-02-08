fe5c4fd9708a4bdcfd7c2adbe5e7c711d8580c86
Simeon Emilov Ivanov <ge65pid@mytum.de>
2025-02-08 15:14:56 +0100

    Downloaded datasets marked as downloaded

--- frontend/src/components/submission/DatasetTable.js
+++ frontend/src/components/submission/DatasetTable.js
@@ -1,9 +1,10 @@
 import React from "react";
-import {Box, Checkbox, Typography} from "@mui/joy";
+import { Box, Checkbox, Typography } from "@mui/joy";
 
 const DatasetsTable = ({
                            datasets,
                            selectedDatasets,
+                           downloadedDatasets,
                            handleToggleSelect,
                            handleSelectAll,
                        }) => {
@@ -12,7 +13,7 @@ const DatasetsTable = ({
             <Box sx={{ display: "flex", padding: "8px 16px", borderBottom: "1px solid #e0e0e0", fontWeight: "bold" }}>
                 <Box sx={{ flex: 1 }}>Dataset</Box>
                 <Box sx={{ flex: 1 }}>Status</Box>
-                <Box sx={{ flex: 1, textAlign: "end"}}>Actions</Box>
+                <Box sx={{ flex: 1, textAlign: "end" }}>Actions</Box>
             </Box>
 
             {datasets.map((dataset, index) => (
@@ -30,10 +31,10 @@ const DatasetsTable = ({
                     </Box>
 
                     <Box sx={{ flex: 1 }}>
-                        {"Not Downloaded"}
+                        {downloadedDatasets.has(dataset.name) ? "Downloaded" : "Not Downloaded"}
                     </Box>
 
-                    <Box sx={{ flex: 1, textAlign: "end"}}>
+                    <Box sx={{ flex: 1, textAlign: "end" }}>
                         <Checkbox
                             checked={selectedDatasets.includes(dataset.name)}
                             onChange={() => handleToggleSelect(dataset.name)}
@@ -43,7 +44,7 @@ const DatasetsTable = ({
             ))}
 
             <Box sx={{ display: "flex", padding: "8px 16px", borderTop: "1px solid #e0e0e0", alignItems: "end", justifyContent: "flex-end" }}>
-                <Box sx={{ display: "flex", textAlign: "end"}}>
+                <Box sx={{ display: "flex", textAlign: "end" }}>
                     <Typography level="body2" sx={{ marginRight: 3 }}>
                         Select All
                     </Typography>
--- frontend/src/components/submission/DownloadStep.js
+++ frontend/src/components/submission/DownloadStep.js
@@ -1,42 +1,14 @@
-import React, {useState, useEffect} from "react";
-import {Box, Button, Stack, Typography} from "@mui/joy";
-import {CloudDownload, InfoOutlined} from "@mui/icons-material";
-import {useSnackbar} from "../../contexts/SnackbarProvider";
+import React, { useState } from "react";
+import { Box, Button, Stack, Typography } from "@mui/joy";
+import { CloudDownload, InfoOutlined } from "@mui/icons-material";
+import { useSnackbar } from "../../contexts/SnackbarProvider";
 import DatasetsTable from "./DatasetTable";
-import {DatasetService} from "../../services/DatasetService";
+import { DatasetService } from "../../services/DatasetService";
 
-const DownloadStep = ({onDatasetDownloaded, onDatasetsFetched}) => {
-    const [datasets, setDatasets] = useState([]);
+const DownloadStep = ({ datasets }) => {
     const [selectedDatasets, setSelectedDatasets] = useState([]);
-    const {showSnackbar} = useSnackbar();
-
-    useEffect(() => {
-        const fetchDatasets = async () => {
-            try {
-                const response = await fetch("http://localhost:5000/datasets/list", {
-                    credentials: "include",
-                });
-                if (!response.ok) {
-                    const errorData = await response.json();
-                    console.error("Failed to fetch datasets:", errorData.error);
-                    return;
-                }
-                const data = await response.json();
-                setDatasets(data.datasets);
-
-                // Pre-select all datasets by default
-                const preselectedDatasets = data.datasets.map((dataset) => dataset.name);
-                setSelectedDatasets(preselectedDatasets);
-
-                onDatasetsFetched(data.datasets);
-            } catch (error) {
-                showSnackbar("Failed to fetch datasets", "error");
-            }
-        };
-        fetchDatasets();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
+    const [downloadedDatasets, setDownloadedDatasets] = useState(new Set());
+    const { showSnackbar } = useSnackbar();
 
     const handleToggleSelect = (datasetName) => {
         setSelectedDatasets((prevSelected) =>
@@ -47,74 +19,62 @@ const DownloadStep = ({onDatasetDownloaded, onDatasetsFetched}) => {
     };
 
     const handleSelectAll = (checked) => {
-        if (checked) {
-            const selectableDatasets = datasets
-                .map((dataset) => dataset.name);
-            setSelectedDatasets(selectableDatasets);
-        } else {
-            setSelectedDatasets([]);
-        }
+        setSelectedDatasets(checked ? datasets.map((dataset) => dataset.name) : []);
     };
 
     const handleDownloadSelected = () => {
         DatasetService.downloadDatasets(selectedDatasets)
             .then(() => {
-                showSnackbar("All selected datasets were downloaded successfully!", "success");
+                showSnackbar("Selected datasets were downloaded successfully!", "success");
+                setDownloadedDatasets((prev) => new Set([...prev, ...selectedDatasets]));
+                setSelectedDatasets([]);
             })
-            .catch((error) => {
-
+            .catch(() => {
                 showSnackbar("Error downloading datasets", "error");
             });
     };
 
-
     return (
         <Box>
-            <Typography level="h2" mb={2} sx={{textAlign: "start"}}>
+            <Typography level="h2" mb={2} sx={{ textAlign: "start" }}>
                 Download Datasets
             </Typography>
 
-            <Typography sx={{marginY: 1, }} level="body1">
-                <Stack spacing={2}>
-                    <Typography>
-                        Please download all the original datasets provided. The datasets contain sensitive
-                        information, and your goal is to apply a data privatization method of your choice to ensure the
-                        privacy and security of the data.
-                    </Typography>
+            <Stack spacing={2} sx={{ marginY: 1 }} level="body1">
+                <Typography>
+                    Please download all the original datasets provided. The datasets contain sensitive
+                    information, and your goal is to apply a data privatization method of your choice to ensure the
+                    privacy and security of the data.
+                </Typography>
+                <Typography>
+                    Once the privatization process is complete, save the resulting privatized datasets. You will
+                    need to upload them in the final step of this workflow.
+                </Typography>
+            </Stack>
 
-                    <Typography>
-                        Once the privatization process is complete, save the resulting privatized datasets. You will
-                        need to upload them in the final step of this workflow.
-                    </Typography>
-                </Stack>
+            <Typography sx={{ marginY: 1, p: 1 }} variant="soft" color="neutral" level="body1">
+                <Typography sx={{ fontWeight: 'bold' }}>
+                    Important: Replace the textual data column with the privatized data while keeping all other columns unchanged and in their original order. This ensures that the benchmarking process functions correctly and handles the datasets as intended.
+                </Typography>
             </Typography>
-
-
-            <Typography sx={{marginY: 1, p: 1}} variant="soft" color="neutral" level="body1">
-                <Stack spacing={2}>
-
-                    <Typography sx={{ fontWeight: 'bold' }}>
-                        Important: Replace the textual data column with the privatized data while keeping all other columns unchanged and in their original order. This ensures that the benchmarking process functions correctly and handles the datasets as intended.
-                    </Typography>
-                </Stack>
-            </Typography>
-
 
             <DatasetsTable
                 datasets={datasets}
                 selectedDatasets={selectedDatasets}
+                downloadedDatasets={downloadedDatasets}
                 handleToggleSelect={handleToggleSelect}
                 handleSelectAll={handleSelectAll}
             />
-            <Typography sx={{p: 1}} startDecorator={<InfoOutlined/>} variant="soft" color="neutral" level="body1">
+
+            <Typography sx={{ p: 1 }} startDecorator={<InfoOutlined />} variant="soft" color="neutral" level="body1">
                 Please make sure you have downloaded the datasets before proceeding to the next step!
             </Typography>
 
-            <Box sx={{mt: 3, textAlign: "center"}}>
+            <Box sx={{ mt: 3, textAlign: "center" }}>
                 <Button
                     fullWidth
                     variant="solid"
-                    startDecorator={<CloudDownload/>}
+                    startDecorator={<CloudDownload />}
                     onClick={handleDownloadSelected}
                     disabled={selectedDatasets.length === 0}
                 >
@@ -126,9 +86,3 @@ const DownloadStep = ({onDatasetDownloaded, onDatasetsFetched}) => {
 };
 
 export default DownloadStep;
-
-
-
-
-
-
