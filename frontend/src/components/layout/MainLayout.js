fc7937096dc8f7022a47362e9c58919e488a1743
Stephen Meisenbacher <sjmeis@gtgd.com>
2026-02-12 12:30:57 +0100

    [skip ci] more footer updates

--- frontend/src/components/layout/MainLayout.js
+++ frontend/src/components/layout/MainLayout.js
@@ -3,7 +3,13 @@ import Footer from "../shared/Footer";
 
 const MainLayout = ({ children }) => {
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+    <Box sx={{ display: "flex",
+                flexDirection: "column",
+                minHeight: "100vh",
+                width: "100vw",
+                margin: 0,
+                padding: 0,
+            }}>
       <Box component="main" sx={{ flex: 1 }}>
         {children}
       </Box>
